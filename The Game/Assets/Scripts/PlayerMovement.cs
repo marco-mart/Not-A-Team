171617823f7128215039d675265d7f5f845578a9
frankentank <54920881+frankentank@users.noreply.github.com>
2022-02-25 14:20:32 -0700

    finished the video

--- The Game/Assets/Scripts/PlayerMovement.cs
+++ The Game/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float Speed;
+    [SerializeField] private float Jump;
+    private Rigidbody2D body;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+    }
+    private void Update() {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, Jump);
     }
 }
