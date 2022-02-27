38fbd971e78ab2bf134c597732b58ddb8c46d1a2
Marco <martinez.marco.1027@gmail.com>
2022-02-27 15:00:35 -0700

    Character flips when going left or right now.

--- The Game/Assets/Scripts/PlayerMovement.cs
+++ The Game/Assets/Scripts/PlayerMovement.cs
@@ -11,9 +11,21 @@ public class PlayerMovement : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
     }
     private void Update() {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, body.velocity.y);
+
+        float hInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2( hInput * Speed, body.velocity.y);
+
+        // flip John Smith left or right depending on
+        // which direction he is going
+        if (hInput > 0) {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (hInput < 0) {
+            transform.localScale = new Vector2(-1, 1);
+        }
 
         if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, Jump);
+        
     }
 }
