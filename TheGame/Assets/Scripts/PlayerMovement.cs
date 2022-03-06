3ad2ed0f11e243f4d44aa167597a595aca993aa0
Marco <martinez.marco.1027@gmail.com>
2022-03-06 14:45:57 -0700

    Add wall jumping ability, camera follows John, add wall

--- TheGame/Assets/Scripts/PlayerMovement.cs
+++ TheGame/Assets/Scripts/PlayerMovement.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float Speed;
-    [SerializeField] private float Jump;
+    [SerializeField] private float jumpPower;
     [SerializeField] private LayerMask groundLayer;
+      [SerializeField] private LayerMask wallLayer;
     private Rigidbody2D body;
     private Animator anim;
     private BoxCollider2D boxCollider;
+    private float wallJumpCoolDown;
+    private float horizontalInput;
     
 
     private void Awake()
@@ -17,32 +20,66 @@ public class PlayerMovement : MonoBehaviour
         anim = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider2D>();
     }
+
     private void Update() {
 
-        float hInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2( hInput * Speed, body.velocity.y);
+        horizontalInput = Input.GetAxis("Horizontal");
 
         // flip John Smith left or right depending on
         // which direction he is going
-        if (hInput > 0) 
+        if (horizontalInput > 0) 
             transform.localScale = new Vector2(1, 1);
-        else if (hInput < 0)
+        else if (horizontalInput < 0)
             transform.localScale = new Vector2(-1, 1);
 
-        //can only jump if grounded, can jump with space or up arrow
-        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)) && isGrounded()) 
-            jump();
-
         //set animator parameters
-        anim.SetBool("run", hInput != 0);
+        anim.SetBool("run", horizontalInput != 0);
         anim.SetBool("grounded", isGrounded());
+
+        // wall jump logic
+        if (wallJumpCoolDown > 0.2f) {
+            
+            body.velocity = new Vector2( horizontalInput * Speed, body.velocity.y);
+
+            if (onWall() && !isGrounded()) {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+            else {
+                body.gravityScale = 1;
+            }
+
+            //can only jump if grounded, can jump with space or up arrow
+            if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))) {
+                jump();
+            }
+        }
+        else {
+            wallJumpCoolDown += Time.deltaTime;
+        }
         
     } //end Update
 
     private void jump()
     {
-        body.velocity = new Vector2(body.velocity.x, Jump);
-        anim.SetTrigger("jump");
+        if (isGrounded()) {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            anim.SetTrigger("jump");
+        }
+        else if (onWall() && !isGrounded()) {
+
+            if (horizontalInput == 0) {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector2(-Mathf.Sign(transform.localScale.x), transform.localScale.y);
+            }
+            else {
+                // wall jump in opposite direction as wall 
+                // ex. left and up or right and up
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 10);
+            }
+
+            wallJumpCoolDown = 0;
+        }
 
     } //end jump
 
@@ -53,6 +90,12 @@ public class PlayerMovement : MonoBehaviour
     private bool isGrounded()
     {
         RaycastHit2D raycast = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycast.collider != null;
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D raycast = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
         return raycast.collider != null;
     }
 }
